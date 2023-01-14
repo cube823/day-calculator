20c9bc983a8a81a58bc4ed113f8a7b6daca921ce
minkyYoon <cube823@gmail.com>
2023-01-14 15:18:37 +0900

    feat: 간단한 로직 추가

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -1,24 +1,41 @@
 import { Inter } from "@next/font/google";
-import { BColor } from "components/ui/atoms/BColor";
 import BFlex from "components/ui/atoms/BFlex";
 import BTextInput from "components/ui/atoms/BTextInput";
 import BTypography from "components/ui/atoms/BTypography";
-import { ChangeEvent, useState } from "react";
+import dayjs from "dayjs";
+import { ChangeEvent, useCallback, useMemo, useState } from "react";
 import styled from "styled-components";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const today = new Intl.DateTimeFormat("ko-KR").format();
-
 const D_DAY_CALCULATOR_TEXT = "디데이 계산기";
-const FROM_NOW_ON_TEXT = "오늘로부터 며칠인지 계산";
+
+const inter = Inter({ subsets: ["latin"] });
+const today = new Intl.DateTimeFormat("ko", { dateStyle: "full" });
 
 const HomeComponent = () => {
   const [fromNowOnDate, setFromNowOnDate] = useState("");
 
-  const onChangeFromNowOnDate = (e: ChangeEvent<HTMLInputElement>) => {
-    setFromNowOnDate(e.currentTarget.value);
-  };
+  const onChangeFromNowOnDate = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setFromNowOnDate(e.currentTarget.value);
+    },
+    []
+  );
+
+  const targetDate = useMemo(() => {
+    const numerable = parseInt(fromNowOnDate, 10);
+    if (!Number.isNaN(numerable)) {
+      return dayjs(new Date())
+        .add(numerable, "days")
+        .format("YYYY. MM. DD.")
+        .replace(".", "년")
+        .replace(".", "월")
+        .replace(".", "일");
+    }
+
+    return "";
+  }, [fromNowOnDate]);
+
+  console.log("targetDate", targetDate);
 
   return (
     <MainContainer className={inter.className}>
@@ -32,24 +49,61 @@ const HomeComponent = () => {
       </HeaderComponent>
 
       <ContentComponent>
-        <BFlex>{today}</BFlex>
+        <BTypography
+          text={`오늘은 ${today.format()} 입니다.`}
+          color="mono08"
+          size={20}
+        />
 
-        <BFlex>
-          <BTypography
-            text={FROM_NOW_ON_TEXT}
-            size={20}
-            color="mono08"
-            isBold
-          />
-
-          <BFlex>
+        <BFlex isColumn>
+          <BFlex alignItems="center" gap={8}>
+            <BTypography text={"오늘로부터"} size={20} color="mono08" isBold />
             <BTextInput
               textInputSizeType={"lg"}
               value={fromNowOnDate}
               onChange={onChangeFromNowOnDate}
               type={"text"}
+              width={240}
               placeholder={"예: "}
+              isFullTextInputWidth
             />
+            <BTypography
+              text={"일 째 되는 날은?"}
+              size={20}
+              color="mono08"
+              isBold
+            />
+            <BTypography text={targetDate} size={20} color="mono08" isBold />
+          </BFlex>
+
+          <BFlex alignItems="center" gap={8}>
+            <BTypography text={"기준일"} size={20} color="mono08" isBold />
+            <BTextInput
+              textInputSizeType={"lg"}
+              value={fromNowOnDate}
+              onChange={onChangeFromNowOnDate}
+              type={"text"}
+              width={240}
+              placeholder={"예: "}
+              isFullTextInputWidth
+            />
+            <BTypography text={"로 부터"} size={20} color="mono08" isBold />
+            <BTextInput
+              textInputSizeType={"lg"}
+              value={fromNowOnDate}
+              onChange={onChangeFromNowOnDate}
+              type={"text"}
+              width={240}
+              placeholder={"예: "}
+              isFullTextInputWidth
+            />
+            <BTypography
+              text={"일 째 되는 날은?"}
+              size={20}
+              color="mono08"
+              isBold
+            />
+            <BTypography text={targetDate} size={20} color="mono08" isBold />
           </BFlex>
         </BFlex>
       </ContentComponent>
@@ -80,8 +134,8 @@ const ContentComponent = styled.div`
   display: flex;
   flex-direction: column;
   width: 960px;
+  flex-wrap: wrap;
   min-height: calc(100vh - 3rem);
-  // background-color: ${BColor.primary01};
   gap: 1rem;
 `;
 
